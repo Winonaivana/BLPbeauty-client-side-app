5d5e415ad2d631264def0725d99f0113a4200439
winona ivana <winonaivana@gmail.com>
2023-07-15 09:45:42 +0100

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import "./App.css";
-import Register from "./Register";
-
-function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="register" element={<Register></Register>} />
-      </Routes>
-    </>
-  );
-}
+const App: React.FC = () => {
+  return <></>;
+};
 
 export default App;
